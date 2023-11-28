223d9448d9b2c976ba8865b1be2467c89a940851
skpntsv <n.skopintsev@g.nsu.ru>
2023-11-28 17:02:11 +0700

    update

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,6 +6,6 @@ module ru.nsu.skopintsev.game {
     requires org.kordamp.bootstrapfx.core;
     requires com.almasb.fxgl.all;
 
-    opens ru.nsu.skopintsev.game to javafx.fxml;
-    exports ru.nsu.skopintsev.game;
+    exports ru.nsu.skopintsev.game.view;
+    opens ru.nsu.skopintsev.game.view to javafx.fxml;
 }
