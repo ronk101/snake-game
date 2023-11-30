bfe46ea275f10550851f7c711c76f8fc3e622ede
skpntsv <n.skopintsev@g.nsu.ru>
2023-11-30 13:26:49 +0700

    add protobuf

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,8 +4,11 @@ module ru.nsu.skopintsev.game {
 
     requires org.controlsfx.controls;
     requires org.kordamp.bootstrapfx.core;
-    requires com.almasb.fxgl.all;
+    requires java.desktop;
+    requires protobuf.java;
 
+    exports ru.nsu.skopintsev.game;
+    opens ru.nsu.skopintsev.game to javafx.fxml;
     exports ru.nsu.skopintsev.game.view;
     opens ru.nsu.skopintsev.game.view to javafx.fxml;
 }
