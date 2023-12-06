e5ab76eb6da26a107d9d7542d31a1651108a73f4
skpntsv <n.skopintsev@g.nsu.ru>
2023-12-06 21:26:04 +0700

    make move of head of snake

--- src/main/java/ru/nsu/skopintsev/game/model/GameModel.java
+++ src/main/java/ru/nsu/skopintsev/game/model/GameModel.java
@@ -1,37 +1,130 @@
 package ru.nsu.skopintsev.game.model;
 
-import me.ippolitov.fit.snakes.SnakesProto.GameConfig;
-import me.ippolitov.fit.snakes.SnakesProto.GamePlayers;
-import me.ippolitov.fit.snakes.SnakesProto.GameState;
+import lombok.Setter;
+import lombok.extern.log4j.Log4j2;
+import ru.nsu.skopintsev.game.model.core.*;
 
+import java.util.List;
+
+@Log4j2
 public class GameModel {
-    private GameState gameState;
-    private GamePlayers gamePlayers;
-    private GameConfig gameConfig;
+    private final GameState gameState;
+    private final GameConfig gameConfig;
+
+    @Setter
+    private Direction currentDirection;
+    private GamePlayer gamePlayer;  // надо?
+    private List<GamePlayer> gamePlayers; // надо?
+    private NodeRole nodeRole; // надо?
+    private PlayerType playerType; // надо?
+
+    // TODO GameAnnouncement
 
 
-    public GameModel(int width, int height, int food_static, int state_delay_ms) {
-        initGameConfig(width, height, food_static, state_delay_ms);
+    public GameModel(GameConfig gameConfig) {
+        this.gameConfig = gameConfig;
+        this.gameState = new GameState();
+        initializeGame();
     }
 
-    private void initGameConfig(int width, int height, int food_static, int state_delay_ms) {
-        gameConfig = GameConfig.newBuilder()
-                .setWidth(width)
-                .setHeight(height)
-                .setFoodStatic(food_static)
-                .setStateDelayMs(state_delay_ms)
-                .build();
+    private void initializeGame() {
+//        for (int playerId = 1; playerId <= gameConfig.getPlayers().size(); playerId++) {
+//            Snake snake = new Snake(playerId, gameConfig.getInitialSnakeX(), gameConfig.getInitialSnakeY());
+//            snakes.add(snake);
+//        }
+
+        // Спавним начальное количество еды
+        spawnFood(gameConfig.getFoodStatic());
     }
 
-    public GameState getGameState() {
-        return gameState;
+    private void spawnFood(int foodCount) {
+//        Random random = new Random();
+//        for (int i = 0; i < foodCount; i++) {
+//            int foodX = random.nextInt(gameConfig.getWidth());
+//            int foodY = random.nextInt(gameConfig.getHeight());
+//            //foods.add(new GameState.Coord(foodX, foodY));
+//            gameState.setFoods(gameState.getFoods().add(new Coord(foodX, foodY)));
+//        }
     }
 
-    public GamePlayers getGamePlayers() {
-        return gamePlayers;
+    public void moveSnake(int playerID, Direction direction) {
+        Snake snake = null;
+        for (Snake snake1: gameState.getSnakes()) {
+            if (snake1.getPlayerID() == playerID) {
+                snake = snake1;
+            }
+            else {
+                log.error("Такого игрока нет " + playerID);
+                return;
+            }
+        }
+        assert snake != null;
+        Direction oldDirection = snake.getHeadDirection();
+        switch (direction) {
+            case UP -> snake.setHeadDirection(Direction.UP);
+            case DOWN -> snake.setHeadDirection(Direction.DOWN);
+            case LEFT -> snake.setHeadDirection(Direction.LEFT);
+            case RIGHT -> snake.setHeadDirection(Direction.RIGHT);
+        }
+
+        Coord newHeadCoord = snake.getPoints().get(0);
+        switch (snake.getHeadDirection()) {
+            case UP -> {
+                if (oldDirection == Direction.DOWN) {
+                    newHeadCoord.setY(((newHeadCoord.getY() + 1) % gameConfig.getHeight()
+                            + gameConfig.getHeight()) % gameConfig.getHeight());
+                    snake.setHeadDirection(Direction.DOWN);
+                }
+                else {
+                    newHeadCoord.setY(((newHeadCoord.getY() - 1) % gameConfig.getHeight()
+                            + gameConfig.getHeight()) % gameConfig.getHeight());
+                }
+            }
+            case DOWN -> {
+                if (oldDirection == Direction.UP) {
+                    newHeadCoord.setY(((newHeadCoord.getY() - 1) % gameConfig.getHeight()
+                            + gameConfig.getHeight()) % gameConfig.getHeight());
+                    snake.setHeadDirection(Direction.UP);
+                }
+                else {
+                    newHeadCoord.setY(((newHeadCoord.getY() + 1) % gameConfig.getHeight()
+                            + gameConfig.getHeight()) % gameConfig.getHeight());
+                }
+            }
+            case LEFT -> {
+                if (oldDirection == Direction.RIGHT) {
+                    newHeadCoord.setX(((newHeadCoord.getY() + 1) % gameConfig.getWidth()
+                            + gameConfig.getWidth()) % gameConfig.getWidth());
+                    snake.setHeadDirection(Direction.RIGHT);
+                }
+                else {
+                    newHeadCoord.setX(((newHeadCoord.getY() - 1) % gameConfig.getWidth()
+                            + gameConfig.getWidth()) % gameConfig.getWidth());
+                }
+            }
+            case RIGHT -> {
+                if (oldDirection == Direction.LEFT) {
+                    newHeadCoord.setX(((newHeadCoord.getY() - 1) % gameConfig.getWidth()
+                            + gameConfig.getWidth()) % gameConfig.getWidth());
+                    snake.setHeadDirection(Direction.RIGHT);
+                }
+                else {
+                    newHeadCoord.setX(((newHeadCoord.getY() + 1) % gameConfig.getWidth()
+                            + gameConfig.getWidth()) % gameConfig.getWidth());
+                }
+            }
+        }
+
+        moveHead(snake, newHeadCoord);
+        moveTail(snake);
+        // TODO сделать коллизию
     }
 
-    public GameConfig getGameConfig() {
-        return gameConfig;
+    private void moveTail(Snake snake) {
+        //TODO сделать сдвиг хвоста
+    }
+
+    private void moveHead(Snake snake, Coord newHeadCoord) {
+        // TODO Сделать сдвиг головы
     }
 }
