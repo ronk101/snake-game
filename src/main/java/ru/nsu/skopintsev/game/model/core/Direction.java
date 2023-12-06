d0ce1b94b087d24751105b61be1a4e7a8b73ccce
skpntsv <n.skopintsev@g.nsu.ru>
2023-12-06 17:37:11 +0700

    make like in proto

--- src/main/java/ru/nsu/skopintsev/game/model/core/Direction.java
+++ src/main/java/ru/nsu/skopintsev/game/model/core/Direction.java
@@ -1,8 +1,25 @@
 package ru.nsu.skopintsev.game.model.core;
 
+import lombok.Getter;
+
 public enum Direction {
-    UP,
-    DOWN,
-    LEFT,
-    RIGHT
+    UP(1),
+    DOWN(2),
+    LEFT(3),
+    RIGHT(4);
+
+    @Getter
+    private final int value;
+
+    Direction(int value) {
+        this.value = value;
+    }
+    public static Direction valueOf(int value) {
+        for (Direction direction : values()) {
+            if (direction.value == value) {
+                return direction;
+            }
+        }
+        throw new IllegalArgumentException("Invalid Direction value: " + value);
+    }
 }
